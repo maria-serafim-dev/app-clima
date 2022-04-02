b4303286f5220f74dc5393d4fb0b2ac064700dfe
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-04-02 11:56:27 -0300

    Utilizando RecyclerView na previsão de clima por dia

--- app/src/main/java/com/example/appclima/ui/MainActivity.kt
+++ app/src/main/java/com/example/appclima/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appclima.R
+import com.example.appclima.adapter.ClimaDiaAdapter
 import com.example.appclima.adapter.ClimaHoraAdapter
 import com.example.appclima.data.DataSource
 
@@ -13,11 +14,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         carregarRecycleClimaPorHora()
+        carregarRecycleClimaPorDia()
     }
 
     private fun carregarRecycleClimaPorHora() {
-        val recyclerView = findViewById<RecyclerView>(R.id.rv_clima_por_hora)
-        val adapter = ClimaHoraAdapter(this, DataSource().listaClimaHora)
-        recyclerView.adapter = adapter
+        val recyclerViewHora = findViewById<RecyclerView>(R.id.rv_clima_por_hora)
+        val adapterHora = ClimaHoraAdapter(this, DataSource().listaClimaHora)
+        recyclerViewHora.adapter = adapterHora
     }
+
+    private fun carregarRecycleClimaPorDia() {
+        val recyclerViewDia = findViewById<RecyclerView>(R.id.rv_clima_por_dia)
+        val adapterDia = ClimaDiaAdapter(this, DataSource().listaClimaDia)
+        recyclerViewDia.adapter = adapterDia
+    }
+
 }
