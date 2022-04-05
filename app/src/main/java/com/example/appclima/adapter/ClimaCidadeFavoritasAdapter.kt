4d601b058bb062d719e3cbaf03c8912e0e369843
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-04-05 20:21:43 -0300

    Adição de funções nas opções do menu na App Bar

--- app/src/main/java/com/example/appclima/adapter/ClimaCidadeFavoritasAdapter.kt
+++ app/src/main/java/com/example/appclima/adapter/ClimaCidadeFavoritasAdapter.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appclima.R
 import com.example.appclima.model.ClimaCidade
@@ -12,13 +15,21 @@ import com.example.appclima.model.ClimaCidade
 class ClimaCidadeFavoritasAdapter (private val context: Context,
                                    private val listClimaCidade: List<ClimaCidade>): RecyclerView.Adapter<ClimaCidadeFavoritasAdapter.ClimaCidadeFavoritasHolder>() {
 
+
+    var visiliby: Boolean? = null
+
     class ClimaCidadeFavoritasHolder(view: View): RecyclerView.ViewHolder(view) {
         val textNomeCidade: TextView = view.findViewById(R.id.tv_nome_cidade)
         val textTemperaturaAtual: TextView  = view.findViewById(R.id.tv_temperatura_atual)
         val textDescricao: TextView  = view.findViewById(R.id.tv_descricao)
         val textMax: TextView  = view.findViewById(R.id.tv_max)
         val textMin: TextView  = view.findViewById(R.id.tv_min)
-
+        val imgRemocao: ImageView = view.findViewById(R.id.img_remocao)
+        val imgMudarPrioridade: ImageView = view.findViewById(R.id.img_mudar_prioridade)
+        var cardCidades: CardView = view.findViewById(R.id.card_cidade)
+        var layout = cardCidades.layoutParams as LinearLayout.LayoutParams
+        val marginStart = layout.marginStart
+        val marginEnd = layout.marginEnd
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClimaCidadeFavoritasHolder {
@@ -33,9 +44,28 @@ class ClimaCidadeFavoritasAdapter (private val context: Context,
             with(item){
                 textNomeCidade.text = nomeCidade
                 textTemperaturaAtual.text = context.resources.getString(R.string.text_temperatura_atual, temperaturaAtual.toString())
-                textDescricao.text = descricao
-                textMax.text = context.resources.getString(R.string.text_minima, temperaturaMinima.toString())
-                textMin.text = context.resources.getString(R.string.text_maxima, temperaturaMaxima.toString())
+
+                if (visiliby == null){
+                    textDescricao.text = descricao
+                    textMax.text = context.resources.getString(R.string.text_minima, temperaturaMinima.toString())
+                    textMin.text = context.resources.getString(R.string.text_maxima, temperaturaMaxima.toString())
+                } else if (visiliby == true){
+                    imgRemocao.visibility = View.VISIBLE
+                    imgMudarPrioridade.visibility = View.VISIBLE
+                    textDescricao.visibility = View.GONE
+                    textMax.visibility = View.GONE
+                    textMin.visibility = View.GONE
+                    layout.marginStart = 0
+                    layout.marginEnd = 0
+                }else{
+                    imgRemocao.visibility = View.GONE
+                    imgMudarPrioridade.visibility = View.GONE
+                    textDescricao.visibility = View.VISIBLE
+                    textMax.visibility = View.VISIBLE
+                    textMin.visibility = View.VISIBLE
+                    layout.marginStart = marginStart
+                    layout.marginEnd = marginEnd
+                }
             }
         }
     }
@@ -43,4 +73,5 @@ class ClimaCidadeFavoritasAdapter (private val context: Context,
     override fun getItemCount(): Int {
         return  listClimaCidade.size
     }
+
 }
--- app/src/main/java/com/example/appclima/ui/ConfigurarCidadesActivity.kt
+++ app/src/main/java/com/example/appclima/ui/ConfigurarCidadesActivity.kt
@@ -1,22 +1,56 @@
 package com.example.appclima.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appclima.R
 import com.example.appclima.adapter.ClimaCidadeFavoritasAdapter
 import com.example.appclima.data.DataSource
+import com.google.android.material.appbar.MaterialToolbar
+
 
 class ConfigurarCidadesActivity : AppCompatActivity() {
+    private lateinit var adapter: ClimaCidadeFavoritasAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configurar_cidades)
 
         carregarRecyclerClimaCidadesFavoritas()
         ouvintePesquisa()
+        ouvinteMenu()
     }
+
+    private fun ouvinteMenu() {
+        val menu: MaterialToolbar = findViewById(R.id.topAppBar)
+
+        menu.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.menu_editar_lista->{
+                    adapter.visiliby = true
+                    adapter.notifyDataSetChanged()
+                    true
+                }
+                R.id.menu_notificacoes->{
+                    adapter.visiliby = false
+                    adapter.notifyDataSetChanged()
+                    true
+                }
+                R.id.menu_fahrenheit -> {
+                    it.isChecked = true
+                    true
+                }
+                R.id.menu_celsius-> {
+                    it.isChecked = true
+                    true
+                }
+                else -> false
+            }
+
+        }
+    }
+
 
     private fun ouvintePesquisa() {
         val textFieldProcurar: TextView = findViewById(R.id.tv_titulo_tempo)
@@ -28,7 +62,8 @@ class ConfigurarCidadesActivity : AppCompatActivity() {
 
     private fun carregarRecyclerClimaCidadesFavoritas() {
         val recyclerView = findViewById<RecyclerView>(R.id.rv_clima_cidades_favoritas)
-        val adapter = ClimaCidadeFavoritasAdapter(this, DataSource().listaClimaCidadeFavorita)
+        adapter = ClimaCidadeFavoritasAdapter(this, DataSource().listaClimaCidadeFavorita)
+
         recyclerView.adapter = adapter
     }
 }
