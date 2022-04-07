494595fbb9316fb04e69dcb995910aec48ca8cb1
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-04-07 19:39:07 -0300

    Realizando teste no carregamento do recyclerview na ConfigurarCidadesActivity.

--- app/src/androidTest/java/com/example/appclima/TesteDaConfigurarCidadesActivity.kt
+++ app/src/androidTest/java/com/example/appclima/TesteDaConfigurarCidadesActivity.kt
@@ -7,7 +7,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import com.example.appclima.ui.MainActivity
+import com.example.appclima.ui.ConfigurarCidadesActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,24 +15,16 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
-class TestesDaActivityMain {
+class TesteDaConfigurarCidadesActivity {
 
     @get:Rule
-    var activityRule: ActivityScenarioRule<MainActivity>
-            = ActivityScenarioRule(MainActivity::class.java)
+    var activityRule: ActivityScenarioRule<ConfigurarCidadesActivity>
+            = ActivityScenarioRule(ConfigurarCidadesActivity::class.java)
 
     @Test
-    fun recyclerViewClimaPorHoraCarregada() {
-        onView(withId(R.id.rv_clima_por_hora))
+    fun recyclerViewCidadesFavoritasCarregada() {
+        onView(withId(R.id.rv_clima_cidades_favoritas))
             .check(matches(isDisplayed()))
     }
-
-    @Test
-    fun recyclerViewClimaPorDiaCarregada() {
-        onView(withId(R.id.rv_clima_por_dia))
-            .check(matches(isDisplayed()))
-    }
-
-
 
 }
