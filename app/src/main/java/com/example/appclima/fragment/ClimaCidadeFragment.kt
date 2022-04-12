09327ab2cd0f27eb8e899bb4aa0ed79494766fda
maria-serafim-dev <maria.serafim.dev@gmail.com>
2022-04-12 14:50:55 -0300

    Usando o View Binding para interagir com visualizações

--- app/src/main/java/com/example/appclima/fragment/ClimaCidadeFragment.kt
+++ app/src/main/java/com/example/appclima/fragment/ClimaCidadeFragment.kt
@@ -4,76 +4,67 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.RecyclerView
 import com.example.appclima.R
 import com.example.appclima.adapter.ClimaDiaAdapter
 import com.example.appclima.adapter.ClimaHoraAdapter
 import com.example.appclima.data.DataSource
+import com.example.appclima.databinding.FragmentViewPageConteudoBinding
 import com.example.appclima.model.ClimaCidade
 
 class ClimaCidadeFragment(private val cidade: ClimaCidade) : Fragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        carregarDadosCidade(view)
-        carregarRecyclerClimaPorHora(view)
-        carregarRecyclerClimaPorDia(view)
-    }
+    private var _binding: FragmentViewPageConteudoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_view_page_conteudo, container, false)
+    ): View {
+        _binding = FragmentViewPageConteudoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    private fun carregarDadosCidade(view: View) {
-        val textNomeCidade: TextView = view.findViewById(R.id.tv_nome_cidade)
-        val textTemperaturaAtual: TextView = view.findViewById(R.id.tv_temperatura_atual)
-        val textDescricao: TextView = view.findViewById(R.id.tv_descricao)
-        val textMax: TextView = view.findViewById(R.id.tv_max)
-        val textMin: TextView = view.findViewById(R.id.tv_min)
-        val textUmidade: TextView = view.findViewById(R.id.tv_umidade)
-        val textSensacao: TextView = view.findViewById(R.id.tv_sensacao)
-        val textChuva: TextView = view.findViewById(R.id.tv_chuva)
-        val textVisibilidade: TextView = view.findViewById(R.id.tv_visibilidade)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        carregarDadosCidade()
+        carregarRecyclerClimaPorHora(view)
+        carregarRecyclerClimaPorDia(view)
+    }
 
-        with(cidade) {
-            textNomeCidade.text = nomeCidade
-            textTemperaturaAtual.text =
-                resources.getString(R.string.text_temperatura_atual, temperaturaAtual.toString())
-            textDescricao.text = descricao
-            textMax.text = resources.getString(R.string.text_minima, temperaturaMinima.toString())
-            textMin.text = resources.getString(R.string.text_maxima, temperaturaMaxima.toString())
-            val textoUmidade = "$umidade%"
-            textUmidade.text = textoUmidade
-            textSensacao.text =
-                resources.getString(R.string.text_valor_sensacao, sensacao.toString())
-            textVisibilidade.text =
-                resources.getString(R.string.text_valor_visibilidade, visibilidade.toString())
-            textChuva.text = resources.getString(
-                R.string.text_valor_chuva_numero,
-                chuvaUtlimasTresHoras.toString()
-            )
+    private fun carregarDadosCidade() {
+        with(binding) {
+            with(cidade) {
+                tvNomeCidade.text = nomeCidade
+                tvTemperaturaAtual.text = resources.getString(
+                        R.string.text_temperatura_atual,
+                        temperaturaAtual.toString()
+                    )
+                tvDescricao.text = descricao
+                tvMax.text = resources.getString(R.string.text_minima, temperaturaMinima.toString())
+                tvMin.text = resources.getString(R.string.text_maxima, temperaturaMaxima.toString())
+                val textoUmidade = "$umidade%"
+                tvUmidade.text = textoUmidade
+                tvSensacao.text = resources.getString(R.string.text_valor_sensacao, sensacao.toString())
+                tvVisibilidade.text = resources.getString(R.string.text_valor_visibilidade, visibilidade.toString())
+                tvChuva.text = resources.getString(
+                    R.string.text_valor_chuva_numero,
+                    chuvaUtlimasTresHoras.toString()
+                )
+            }
         }
     }
 
     private fun carregarRecyclerClimaPorHora(view: View) {
-        val recyclerViewHora = view.findViewById<RecyclerView>(R.id.rv_clima_por_hora)
         val adapterHora = ClimaHoraAdapter(view.context, DataSource().listaClimaHora)
-        recyclerViewHora.adapter = adapterHora
-
+        binding.rvClimaPorHora.adapter = adapterHora
     }
 
     private fun carregarRecyclerClimaPorDia(view: View) {
-        val recyclerViewDia = view.findViewById<RecyclerView>(R.id.rv_clima_por_dia)
         val adapterDia = ClimaDiaAdapter(view.context, DataSource().listaClimaDia)
-        recyclerViewDia.adapter = adapterDia
-        recyclerViewDia.addItemDecoration(
+        binding.rvClimaPorDia.adapter = adapterDia
+        binding.rvClimaPorDia.addItemDecoration(
             DividerItemDecoration(view.context, DividerItemDecoration.VERTICAL)
         )
     }
-
 }
